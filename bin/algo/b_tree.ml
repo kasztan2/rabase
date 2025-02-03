32bcf45c8fb03c7d37c732ae38892f28fdaee658
Krzysiek <338171@uwr.edu.pl>
2025-02-03 19:16:45 +0100

    b-tree kind of done?

--- bin/algo/b_tree.ml
+++ bin/algo/b_tree.ml
@@ -20,12 +20,28 @@ module B_tree (P : PageHandler) : S = struct
     | false -> (
         match P.get_child node x with None -> None | Some c -> find c x)
 
-  (** assumes that there is no such element present already *)
-  let rec insert node x y =
+  let check_and_split node ?two_children k v =
+    match P.fits node k v with
+    | false -> Some (P.split node k v)
+    | true ->
+        P.insert node ?two_children k v;
+        None
+
+  let rec insert_helper node k v =
     match P.is_leaf node with
     | false -> (
-        match P.get_child node x with
-        | Some c -> insert c x y
-        | None -> failwith "what")
-    | true -> P.insert node x y
+        match P.get_child node k with
+        | None -> failwith "what"
+        | Some c -> (
+            match insert_helper c k v with
+            | None -> None
+            | Some (p1, (k, v), p2) ->
+                check_and_split node ?two_children:(Some (p1, p2)) k v))
+    | true -> check_and_split node k v
+
+  (** assumes that there is no such element present already *)
+  let insert node k v =
+    match insert_helper node k v with
+    | None -> ()
+    | Some x -> P.create_new_root x
 end
